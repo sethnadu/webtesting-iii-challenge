aeafb06b2e1e7d15a6c7417db2552210d953c844
Seth Nadu <seth.nadu@gmail.com>
2019-08-19 17:00:56 -0400

    some tests are done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,40 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer'; 
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "./Dashboard"
+
+describe('<Dashboard />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); 
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it('close gate button toggles to open on click', () => {
+    const {getByText, queryByText} = render(<Dashboard/>)
+    const buttonClosed = getByText(/close gate/i)
+
+    expect(queryByText(/close gate/i)).toBeTruthy()
+    expect(queryByText(/Open Gate/i)).toBeFalsy()
+    fireEvent.click(buttonClosed)
+    expect(queryByText(/Open Gate/i)).toBeTruthy()
+    expect(queryByText(/close gate/i)).toBeFalsy()
+    fireEvent.click(buttonClosed)
+    expect(queryByText(/close gate/i)).toBeTruthy()
+    expect(queryByText(/Open Gate/i)).toBeFalsy()
+  });
+  
+  it('lock button toggles to open on click', () => {
+    const {getByText, queryByText} = render(<Dashboard/>)
+    const buttonlocked = getByText(/lock gate/i)
+    const buttonClosed = getByText(/close gate/i)
+
+    fireEvent.click(buttonClosed)
+    expect(queryByText(/lock gate/i)).toBeTruthy()
+    expect(queryByText(/unlock Gate/i)).toBeFalsy()
+    fireEvent.click(buttonlocked)
+    expect(queryByText(/unlock Gate/i)).toBeTruthy()
+    // expect(queryByText(/lock gate/i)).toBeFalsy()
+    fireEvent.click(buttonlocked)
+    expect(queryByText(/lock gate/i)).toBeTruthy()
+    expect(queryByText(/unlock Gate/i)).toBeFalsy()
+  })
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; 
+import { render} from "@testing-library/react";
+
+import Display from "./Display.js";
+
+
+describe('<Display />', () => {
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Display />); 
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it("open unlocked as default states", () => {
+        const { getByText } = render(<Display />)
+        getByText(/unlocked/i)
+        getByText(/locked/i)
+    });
+   
+  });
