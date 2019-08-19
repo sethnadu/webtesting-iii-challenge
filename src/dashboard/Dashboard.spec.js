a816e3690a47364a0a74db283b685f57c1b86be1
Seth Nadu <seth.nadu@gmail.com>
2019-08-19 18:41:35 -0400

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -8,33 +8,93 @@ describe('<Dashboard />', () => {
     const tree = renderer.create(<Dashboard />); 
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+
   it('close gate button toggles to open on click', () => {
     const {getByText, queryByText} = render(<Dashboard/>)
     const buttonClosed = getByText(/close gate/i)
+    // const buttonlocked = getByText(/lock gate/i)
 
+  
     expect(queryByText(/close gate/i)).toBeTruthy()
     expect(queryByText(/Open Gate/i)).toBeFalsy()
+
     fireEvent.click(buttonClosed)
     expect(queryByText(/Open Gate/i)).toBeTruthy()
     expect(queryByText(/close gate/i)).toBeFalsy()
+   
     fireEvent.click(buttonClosed)
     expect(queryByText(/close gate/i)).toBeTruthy()
     expect(queryByText(/Open Gate/i)).toBeFalsy()
   });
-  
+
   it('lock button toggles to open on click', () => {
     const {getByText, queryByText} = render(<Dashboard/>)
     const buttonlocked = getByText(/lock gate/i)
     const buttonClosed = getByText(/close gate/i)
 
     fireEvent.click(buttonClosed)
-    expect(queryByText(/lock gate/i)).toBeTruthy()
-    expect(queryByText(/unlock Gate/i)).toBeFalsy()
+    expect(queryByText(/Lock Gate/)).toBeTruthy()
+    expect(queryByText(/Unlock Gate/)).toBeFalsy()
     fireEvent.click(buttonlocked)
-    expect(queryByText(/unlock Gate/i)).toBeTruthy()
-    // expect(queryByText(/lock gate/i)).toBeFalsy()
+    expect(queryByText(/Unlock Gate/)).toBeTruthy()
+    expect(queryByText(/Lock Gate/)).toBeFalsy()
     fireEvent.click(buttonlocked)
-    expect(queryByText(/lock gate/i)).toBeTruthy()
-    expect(queryByText(/unlock Gate/i)).toBeFalsy() 
+    expect(queryByText(/Lock Gate/)).toBeTruthy()
+    expect(queryByText(/Unlock Gate/)).toBeFalsy() 
   })
+
+
+  it("'close gate' button disables and enables 'lock gate' button", ()=> {
+    const {getByText} = render(<Dashboard/>)
+    const buttonClosed = getByText(/close gate/i)
+    const buttonlocked = getByText(/lock gate/i)
+    expect(buttonlocked.disabled).toBe(true);
+    fireEvent.click(buttonClosed)
+    expect(buttonlocked.disabled).toBe(false);
+  })
+
+
+  it("'lock gate' button disables and enables 'close gate' button", ()=> {
+    const {getByText} = render(<Dashboard/>)
+    const buttonClosed = getByText(/close gate/i)
+    const buttonlocked = getByText(/lock gate/i)
+
+    fireEvent.click(buttonClosed)
+    fireEvent.click(buttonlocked)
+    expect(buttonClosed.disabled).toBe(true);
+    fireEvent.click(buttonlocked)
+    expect(buttonClosed.disabled).toBe(false);
+  })
+
+  it("toggles 'open div' on 'close' button click", () => {
+    const {getByText} = render (<Dashboard />)
+    const closeButton = getByText(/close gate/i)
+    const openDiv = getByText(/open/i)
+
+    expect(openDiv.className).toBe('led green-led')
+    fireEvent.click(closeButton)
+    expect(openDiv.className).toBe('led red-led')
+    fireEvent.click(closeButton)
+    expect(openDiv.className).toBe('led green-led')
+    
+
+  })
+
+  it("toggles 'lock div' on 'lock' button click", () => {
+    const {getByText} = render (<Dashboard />)
+    const closeButton = getByText(/close gate/i)
+    const lockButton = getByText(/lock gate/i)
+    const lockDiv = getByText(/unlocked/i)
+
+    expect(lockDiv.className).toBe('led green-led')
+    fireEvent.click(closeButton)
+    fireEvent.click(lockButton)
+    expect(lockDiv.className).toBe('led red-led')
+    fireEvent.click(lockButton)
+    expect(lockDiv.className).toBe('led green-led')
+    
+
+  })
+
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer'; 
-import { render} from "@testing-library/react";
+import { render, fireEvent} from "@testing-library/react";
 
 import Display from "./Display.js";
 
@@ -13,8 +13,26 @@ describe('<Display />', () => {
 
     it("open unlocked as default states", () => {
         const { getByText } = render(<Display />)
-        getByText(/unlocked/i)
-        getByText(/locked/i)
+        getByText(/Unlocked/)
+        getByText(/open/i)
     });
+    it("Closed if 'closed prop' is 'true' 'open' if otherwise", () => {
+        const {getByText, rerender} = render(<Display locked = {false} closed={false}/>)
+        const open = getByText(/open/i);
+        const unlocked = getByText(/Unlocked/);
+
+        expect(open.className).toBe('led green-led');
+        expect(open.textContent).toBe('Open')
+        expect(unlocked.className).toBe('led green-led');
+        expect(unlocked.textContent).toBe('Unlocked')
+
+        rerender(<Display locked = {true} closed={true}/>)
+        
+        expect(open.className).toBe('led red-led');
+        expect(open.textContent).toBe('Closed')
+        expect(unlocked.className).toBe('led red-led');
+        expect(unlocked.textContent).toBe('Locked')
+
+      })
    
   });
